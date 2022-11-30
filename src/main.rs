542c6e1353e51dc87762d41e4f1340692fb151b3
Moreliaa <eth_er@gmx.de>
2022-11-30 20:55:41 +0100

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
 use std::env;
 use std::fs;
 use std::path::PathBuf;
+use std::collections::HashMap;
 use std::str::FromStr;
 
 fn main() {
     let cookie = read_cookie();
-    println!("{}", cookie);
     let year = "2021";
     let day = "1";
-    let input = get_input(year, day);
+    let input = get_input(year, day, &cookie);
+    println!("{}", input);
 }
 
 fn read_cookie() -> String {
     return fs::read_to_string("cookie.txt").expect("Failed to read cookie.txt");
 }
 
-fn get_input(year:&str, day:&str) -> String {
+fn get_input(year:&str, day:&str, cookie:&str) -> String {
     let input_path = get_input_path(year, day);
-    let result = String::from("temp");
     println!("{}", input_path.display());
-    return result;
-    /*fetch_input_from_aoc(year, day, inputPath);
-    fs.readFile(inputPath, (err, data) => {
-        if (err)
-            fetchInput(year, day, inputPath)
-        else
-            runDay(year, day, data.toString())
-    })*/
+    match fs::read_to_string(&input_path) {
+        Err(_reason) => return fetch_input_from_site(year, day, &input_path, cookie),
+        Ok(value) => return value,
+    };
 }
 
 fn get_input_path(year:&str, day:&str) -> PathBuf {
@@ -40,35 +36,29 @@ fn get_input_path(year:&str, day:&str) -> PathBuf {
     path
 }
 
-/*function fetch_input_from_aoc(year, day, inputPath) {
-    const path = `/${year}/day/${day}/input`
-    const options = {
-        host: 'adventofcode.com',
-        path: path,
-        method: 'GET',
-        headers: {
-            'Cookie': cookie
-        }
+fn fetch_input_from_site(year:&str, day:&str, inputPath:&PathBuf, cookie:&str) -> String {
+    let mut url = String::from("https://www.adventofcode.com/");
+    url.push_str(year);
+    url.push_str("/day/");
+    url.push_str(day);
+    url.push_str("/input");
+
+    /*let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::SET, cookie.parse().unwrap());
+    let client;
+    match reqwest::blocking::Client::builder().cookie_store(true).default_headers(headers).build() {
+        Err(_) => panic!(),
+        Ok(value) => client = value,
+    }*/
+
+    let client = reqwest::blocking::Client::new();
+    let response;
+    match client.get(&url).header("Cookie", cookie).send() {
+        Err(reason) => panic!("{}", reason),
+        Ok(value) => response = value.text(),
     }
-
-    let req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
-        let body = []
-        res.on('data', (d) => {
-            body.push(d)
-        }).on('end', () => {
-            let result = Buffer.concat(body).toString()
-            fs.outputFile(inputPath, result).then(() => {
-                runDay(year, day, result)
-            }).catch(err => {
-                console.error(err)
-            })
-        })
-    })
-
-    req.on('error', (error) => {
-        console.error(error)
-    })
-
-    req.end()
-}*/
+    match response {
+        Err(reason) => panic!("{}", reason),
+        Ok(value) => value
+    }
+}
