6673852b8671c8143218f4a65ba17e9ef86aceb6
Moreliaa <eth_er@gmx.de>
2022-12-26 11:55:25 +0100

    day15

--- src/day15.rs
+++ src/day15.rs
@@ -1,14 +1,15 @@
 use regex::Regex;
-use std::{vec::Vec, cmp::Ordering};
 use std::collections::HashSet;
+use std::{cmp::Ordering, vec::Vec};
 
 pub fn run(input: String) {
     let rx = Regex::new(r"Sensor at x=(.+), y=(.+): closest beacon is at x=(.+), y=(.+)").unwrap();
-    let mut ranges_pt1:Vec<(i32, i32)> = Vec::new();
+    let mut ranges_pt1: Vec<(i32, i32)> = Vec::new();
     //let target_y = 10;
     let target_y = 2000000;
 
-    let mut beacons:HashSet<(i32, i32)> = HashSet::new();
+    let mut beacons: HashSet<(i32, i32)> = HashSet::new();
+    let mut sensors: Vec<(i32, i32, i32, i32)> = Vec::new();
 
     for line in input.lines() {
         let matches = rx.captures(line).unwrap();
@@ -17,15 +18,57 @@ pub fn run(input: String) {
         let x_beacon = matches[3].parse::<i32>().unwrap();
         let y_beacon = matches[4].parse::<i32>().unwrap();
         beacons.insert((x_beacon, y_beacon));
-        let range_pt1 = get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(x_sensor, y_sensor, x_beacon, y_beacon, target_y);
+        sensors.push((x_sensor, y_sensor, x_beacon, y_beacon));
+        let range_pt1 = get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(
+            x_sensor, y_sensor, x_beacon, y_beacon, target_y,
+        );
         if range_pt1 == None {
             continue;
         }
         ranges_pt1.push(range_pt1.unwrap());
     }
-    let joint_ranges_pt1 = join_ranges(&mut ranges_pt1);
-    println!("Pt1: {}", count_joint_total(&joint_ranges_pt1, &beacons, target_y)); // TODO subtract number of beacons within the ranges
-    
+    ranges_pt1.sort_by(|a, b| compare_ranges(a, b));
+    let joint_ranges_pt1 = join_ranges(&ranges_pt1);
+    println!(
+        "Pt1: {}",
+        count_joint_total(&joint_ranges_pt1, &beacons, target_y)
+    );
+
+    let y_max = 4000000;
+    for target_y_pt2 in 0..y_max {
+        if target_y_pt2 % 100000 == 0 {
+            print!(".");
+        }
+        let mut ranges_pt2: Vec<(i32, i32)> = Vec::new();
+        for s in sensors.iter() {
+            let range_pt2 = get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(
+                s.0,
+                s.1,
+                s.2,
+                s.3,
+                target_y_pt2,
+            );
+            if range_pt2 == None {
+                continue;
+            }
+            ranges_pt2.push(range_pt2.unwrap());
+        }
+        ranges_pt2.sort_by(|a, b| compare_ranges(a, b));
+        let joint_ranges_pt2 = join_ranges(&ranges_pt2);
+        let result = check_for_possible_position(&joint_ranges_pt2, y_max);
+        if result != None {
+            let target_x_pt2 = result.unwrap() as i128;
+
+            println!(
+                "Pt2: {},{} -> {}",
+                target_x_pt2,
+                target_y_pt2,
+                target_x_pt2 * 4000000 + target_y_pt2 as i128
+            );
+            break;
+        }
+    }
+
     /*for y in -2..23 {
         let range = get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(8, 7, 2, 10, y);
         for x in -2..26 {
@@ -44,36 +87,32 @@ pub fn run(input: String) {
     }*/
 }
 
-fn manh(x1:i32, y1:i32, x2:i32, y2:i32) -> i32 {
-    return (x1-x2).abs() + (y1-y2).abs();
-}
-
-fn count_joint_total(ranges:&Vec<(i32,i32)>, beacons:&HashSet<(i32, i32)>, y_coord: i32) -> i32 {
-    let mut total = 0;
-    for r in ranges.iter() {
-        total += r.1 - r.0 + 1 - get_number_of_beacons_in_range(*r, &beacons, y_coord);
+fn get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(
+    x_sensor: i32,
+    y_sensor: i32,
+    x_beacon: i32,
+    y_beacon: i32,
+    y_coord: i32,
+) -> Option<(i32, i32)> {
+    let manh_to_closest_beacon = manh(x_sensor, y_sensor, x_beacon, y_beacon);
+    let distance_to_y_coord = (y_sensor - y_coord).abs();
+    let x_range = manh_to_closest_beacon - distance_to_y_coord;
+    if x_range < 0 {
+        return None;
     }
-    return total;
+    return Some((x_sensor - x_range, x_sensor + x_range));
 }
 
-fn get_number_of_beacons_in_range(range:(i32, i32), beacons:&HashSet<(i32, i32)>, y_coord: i32) -> i32 {
-    let mut total = 0;
-    for b in beacons.iter() {
-        if b.1 != y_coord {
-            continue;
-        }
-        if b.0 >= range.0 && b.0 <= range.1 {
-            total += 1;
-        }
+fn manh(x1: i32, y1: i32, x2: i32, y2: i32) -> i32 {
+    return (x1 - x2).abs() + (y1 - y2).abs();
+}
+
+fn join_ranges(ranges: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    let mut new_ranges: Vec<(i32, i32)> = Vec::new();
+    let mut to_skip: HashSet<usize> = HashSet::new();
+    if ranges.len() == 0 {
+        return new_ranges;
     }
-    return total;
-}
-
-fn join_ranges(ranges: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
-    ranges.sort_by(|a, b| compare_ranges(a, b));
-    let mut new_ranges:Vec<(i32, i32)> = Vec::new();
-    let mut to_skip:HashSet<usize> = HashSet::new();
-
     for i in 0..ranges.len() - 1 {
         if to_skip.contains(&i) {
             continue;
@@ -82,7 +121,8 @@ fn join_ranges(ranges: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
         let mut next_range = (r0.0, r0.1);
         for j in i + 1..ranges.len() {
             let r1 = ranges.get(j).unwrap();
-            if r1.0 <= next_range.1 { // overlap
+            if r1.0 <= next_range.1 {
+                // overlap
                 if r1.1 > next_range.1 {
                     next_range.1 = r1.1;
                 }
@@ -96,7 +136,7 @@ fn join_ranges(ranges: &mut Vec<(i32, i32)>) -> Vec<(i32, i32)> {
     return new_ranges;
 }
 
-fn compare_ranges(a:&(i32, i32), b:&(i32, i32)) -> Ordering {
+fn compare_ranges(a: &(i32, i32), b: &(i32, i32)) -> Ordering {
     if a.0 < b.0 {
         return Ordering::Less;
     } else {
@@ -104,12 +144,42 @@ fn compare_ranges(a:&(i32, i32), b:&(i32, i32)) -> Ordering {
     }
 }
 
-fn get_range_where_you_cannot_possibly_find_a_beacon_on_y_coordinate(x_sensor: i32, y_sensor: i32, x_beacon: i32, y_beacon: i32, y_coord: i32) -> Option<(i32, i32)> {
-    let manh_to_closest_beacon = manh(x_sensor, y_sensor, x_beacon, y_beacon);
-    let distance_to_y_coord = (y_sensor - y_coord).abs();
-    let x_range = manh_to_closest_beacon - distance_to_y_coord;
-    if x_range < 0 { // off by one error?
-        return None;
+fn count_joint_total(ranges: &Vec<(i32, i32)>, beacons: &HashSet<(i32, i32)>, y_coord: i32) -> i32 {
+    let mut total = 0;
+    for r in ranges.iter() {
+        total += r.1 - r.0 + 1 - get_number_of_beacons_in_range(*r, &beacons, y_coord);
     }
-    return Some((x_sensor - x_range, x_sensor + x_range));
+    return total;
+}
+
+fn get_number_of_beacons_in_range(
+    range: (i32, i32),
+    beacons: &HashSet<(i32, i32)>,
+    y_coord: i32,
+) -> i32 {
+    let mut total = 0;
+    for b in beacons.iter() {
+        if b.1 != y_coord {
+            continue;
+        }
+        if b.0 >= range.0 && b.0 <= range.1 {
+            total += 1;
+        }
+    }
+    return total;
+}
+
+fn check_for_possible_position(ranges: &Vec<(i32, i32)>, y_max: i32) -> Option<i32> {
+    let mut last_idx = 0;
+    for r in ranges {
+        if r.0 > last_idx {
+            return Some(last_idx);
+        } else {
+            last_idx = r.1 + 1;
+            if last_idx > y_max {
+                return None;
+            }
+        }
+    }
+    return None;
 }
