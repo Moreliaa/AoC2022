99957a96c9304191bcd64c9a8e7c60fbd49756ef
Moreliaa <eric@DESKTOP-HFIGKGV>
2023-10-03 15:55:37 +0200

    use aoc-lib

--- src/main.rs
+++ src/main.rs
@@ -36,12 +36,12 @@ mod day15;
 #[allow(dead_code)]
 mod day16;
 
+extern crate aoc_lib;
+
 fn main() {
-    let cookie = read_cookie();
-    //println!("{}", cookie);
     let year = "2022";
-    let day = "16";
-    let input = get_input(year, day, &cookie);
+    let day = "15";
+    let input = aoc_lib::input_reader::get_input(year, day, "cookie.txt");
     //day1::run(input);
     //day2::run(input);
     //day3::run(input);
@@ -56,57 +56,6 @@ fn main() {
     //day12::run(input);
     //day13::run(input);
     //day14::run(input);
-    //day15::run(input);
-    day16::run(input);
-}
-
-fn read_cookie() -> String {
-    return fs::read_to_string("cookie.txt").expect("Failed to read cookie.txt");
-}
-
-fn get_input(year:&str, day:&str, cookie:&str) -> String {
-    let input_path = get_input_path(year, day);
-    //println!("{}", input_path.display());
-    match fs::read_to_string(&input_path) {
-        Err(_reason) => return fetch_input_from_site(year, day, &input_path, cookie),
-        Ok(value) => return value,
-    };
-}
-
-fn get_input_path(year:&str, day:&str) -> PathBuf {
-    let mut path = env::current_dir().expect("Couldn't read current dir."); 
-    path.push("input");
-    let mut yearday = String::from(year);
-    yearday.push_str("_");
-    yearday.push_str(day);
-    path.push(yearday);
-    path.set_extension("txt");
-    path
-}
-
-fn fetch_input_from_site(year:&str, day:&str, _input_path:&PathBuf, cookie:&str) -> String {
-    let mut url = String::from("https://www.adventofcode.com/");
-    url.push_str(year);
-    url.push_str("/day/");
-    url.push_str(day);
-    url.push_str("/input");
-
-    /*let mut headers = reqwest::header::HeaderMap::new();
-    headers.insert(reqwest::header::SET, cookie.parse().unwrap());
-    let client;
-    match reqwest::blocking::Client::builder().cookie_store(true).default_headers(headers).build() {
-        Err(_) => panic!(),
-        Ok(value) => client = value,
-    }*/
-
-    let client = reqwest::blocking::Client::new();
-    let response;
-    match client.get(&url).header("Cookie", cookie).send() {
-        Err(reason) => panic!("{}", reason),
-        Ok(value) => response = value.text(),
-    }
-    match response {
-        Err(reason) => panic!("{}", reason),
-        Ok(value) => value
-    }
+    day15::run(input);
+    //day16::run(input);
 }
